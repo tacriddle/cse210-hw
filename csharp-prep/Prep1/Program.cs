5dfe97ef6fec1b4bdf301ad1b28ea0ad377b7e30
tacriddle <thomas_criddle@byui.edu>
2024-04-25 17:35:17 -0600

    Did the assigment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string tacFirstName= Console.ReadLine();
+        Console.Write("What is your last name?");
+        string tacLastName=Console.ReadLine();
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {tacLastName}, {tacFirstName} {tacLastName}.");
     }
 }
