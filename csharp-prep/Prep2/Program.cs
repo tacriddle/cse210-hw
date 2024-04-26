4947e7864b8fa99202979fd69b557b882e1f2b00
tacriddle <thomas_criddle@byui.edu>
2024-04-25 20:11:14 -0600

    did the assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string tacLetter;
+        Console.Write("What is your persentage in the class?");
+        string tacGradePercentString=Console.ReadLine();
+        float tacGradePercent= int.Parse(tacGradePercentString);
+        if (tacGradePercent>= 90){
+            tacLetter="A";
+        }
+        else if (tacGradePercent >= 80){
+            tacLetter="B";
+        }
+        else if (tacGradePercent>=70){
+            tacLetter="C";
+        }
+        else if (tacGradePercent>=60){
+            tacLetter="d";
+        }
+        else{
+            tacLetter="F";
+        }
+        Console.WriteLine($"Your grade is {tacLetter}.");
+        if (tacGradePercent>=70){
+            Console.WriteLine("Congrats you passed the class");
+        }
+        if (tacGradePercent<70){
+            Console.WriteLine("Sorry you failed the class");
+        }
     }
 }
