9271eb4511ff1f0407b748ea3ed8fa15f338c309
tacriddle <thomas_criddle@byui.edu>
2024-05-01 16:42:41 -0600

    did the prep assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicWord;
+        Console.Write("What is the magic number: ");
+        magicWord=int.Parse(Console.ReadLine());
+        int guess=(0);
+        while (guess != magicWord){
+            Console.Write("Please guess the magic word: ");
+            guess=int.Parse(Console.ReadLine());
+            if (guess>magicWord){
+                Console.WriteLine("Lower.");
+            }
+            else if (guess<magicWord){
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("Congrats you guessed the word!");
+            }
+        }
+        
     }
 }
