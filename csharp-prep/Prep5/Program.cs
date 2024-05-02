29ab0eacbb57d15099a50a48b75f7e021577d614
tacriddle <thomas_criddle@byui.edu>
2024-05-02 10:58:31 -0600

    did prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,41 @@ using System;
 
 class Program
 {
+    static string getUserName(){
+        Console.Write("Please enter your name. ");
+        string userName;
+        userName=Console.ReadLine();
+        return userName;
+    }
+
+    static int getFavoriteNumber(){
+        Console.Write("Please enter your favorite number:");
+        int number=0;
+        number=int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int squareNumber(int number){
+        number=(number*number);
+        return number;
+    }
+
+    static void displayWelcome(){
+
+        Console.WriteLine("Welcome to the program");
+    }
+    static void displayResults(string name, int numberSquared){
+        Console.WriteLine($"{name}, your favorite number squared is {numberSquared}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayWelcome();
+        string name;
+        name=getUserName();
+        int favoriteNumber=0;
+        favoriteNumber=getFavoriteNumber();
+        int doubleNumber=0;
+        doubleNumber=squareNumber(favoriteNumber);
+        displayResults(name, doubleNumber);
     }
 }
