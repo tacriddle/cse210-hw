9523e4d32ec49d125ee8b5b632b1904946fac49a
tacriddle <thomas_criddle@byui.edu>
2024-05-02 10:27:23 -0600

    did prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers=new List<int>();
+        int guess=1;
+        while (guess != 0){
+            Console.Write("Please enter a number, enter 0 when finished. ");
+            guess=int.Parse(Console.ReadLine());
+            numbers.Add(guess);
+        }
+        int numbersSum=numbers.Sum();
+        int average=(numbersSum/(numbers.Count));
+        int largest=0;
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        Console.WriteLine($"The sum is {numbersSum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {largest}");
+
+
     }
 }
